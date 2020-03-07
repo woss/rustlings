5177dd4bb89307d36ad5d84545db91df1182af30
Daniel Maricic <daniel@woss.io>
2020-03-07 10:59:56 +0100

    round 1

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,10 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
     let number = "3";
     println!("Number {}", number);
-    number = 3;
+    // https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html?highlight=sha#shadowing
+    let number = 3;
     println!("Number {}", number);
 }
