bc47e7eb6fb215cf1a47f7bf276cb0b9316da504
Daniel Maricic <daniel@woss.io>
2020-03-20 19:23:33 +0100

    update
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -1,25 +1,26 @@
 // traits2.rs
-// 
+//
 // Your task is to implement the trait
 // `AppendBar' for a vector of strings.
-// 
+//
 // To implement this trait, consider for
 // a moment what it means to 'append "Bar"'
 // to a vector of strings.
-// 
+//
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
-    fn append_bar(self) -> Self;
+    fn append_bar(&mut self) -> Self;
 }
 
 //TODO: Add your code here
-
-
-
+impl AppendBar for Vec<String> {
+    fn append_bar(&mut self) -> Self {
+        self.push(String::from("Bar"));
+        self.to_vec()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,8 +29,8 @@ mod tests {
     #[test]
     fn is_vec_pop_eq_bar() {
         let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo, vec![String::from("Foo"), String::from("Bar")]);
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
-
 }
